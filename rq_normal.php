312380eb69077782a24c66bdf9ebbbabdd5d9148
yiliu <yiliu@yangqianguan.com>
2015-11-04 14:46:08 +0800

    提交一下吧，加了些乱七八糟的

---
+++ rq_normal.php
@@ -0,0 +1,29 @@
+				<div style="top:0px;position:relative;">
+					<div style="padding:20px;">
+						<table class="list-table">
+							<colgroup>
+								<col style="width:80px;">
+								<col style="width:80px;">
+								<col style="width:130px;">
+								<col>
+								<col style="width:110px;">
+							</colgroup>
+							<thead>
+								<tr><td></td><td></td><td></td><td></td><td></td></tr>
+							</thead>
+							<tbody>
+								<tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+								<tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+								<tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+								<tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+								<tr><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<div style="top:0px;position:relative;">
+					<?php
+					$cc = empty($_GET["cc"]) ? 1 : $_GET["cc"];
+					?>
+					<div class="paging-container" style="padding:15px;text-align:right;" tatal="300" current="<?php echo $cc;?>" limit="30" request_url="rq_normal.php"></div>
+				</div>
